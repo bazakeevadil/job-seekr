39ee958993abcc50a496cd66b3b9afe7697b3d3e
abazakeev <bazakeev04@gmail.com>
2023-10-28 18:29:47 +0600

    Adding AppDbContext and DependencyInjection

---
+++ JobSeekr/WebApi/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+
+namespace WebApi;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDbContext(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(opts =>
+            opts.UseSqlServer(
+                configuration.GetConnectionString("Default")));
+
+        return services;
+    }
+}
